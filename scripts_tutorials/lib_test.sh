399dc9c6e2084a7f398f0601dcb8b3baa8e4311e
LiuXiang199x <lx19961026@gmail.com>
2021-10-22 19:29:15 +0800

    2021/10/22

--- scripts_tutorials/lib_test.sh
+++ scripts_tutorials/lib_test.sh
@@ -11,6 +11,7 @@ fi
 rand(){
 	min=$1
 	max=$(($2-$min+1))
+	# $RANDOM: 生成0-32767之间的整数随机数，若超过5位可以加个固定10位整数，然后进行求余。
 	num=$(($RANDOM+1000000000))
 	echo $(($num%$max+$min))
 }
@@ -19,6 +20,7 @@ world_array=(10 10 10 10 10 10 10 10 10 10 10 10 10 10 10 10)
 
 for i in {1..100};do
 	rnd=$(rand 1 16)
+	echo "rnd = $rnd"
 	while ((${world_array[$(($rnd-1))]}<=0));do
 		rnd=$(rand 1 16)
 	done
