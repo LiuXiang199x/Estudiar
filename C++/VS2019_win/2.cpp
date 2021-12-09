68c31063a2f5ef63d9929731e346a073de357d3a
LiuXiang199x <lx19961026@gmail.com>
2021-12-09 11:41:00 +0800

    2021/12/9

--- C++/VS2019_win/2.cpp
+++ C++/VS2019_win/2.cpp
@@ -38,64 +38,6 @@ using namespace cv;
 /*-------------------------------------------
                   Functions
 -------------------------------------------*/
-
-uint64_t time_tToTimestamp(const time_t &t ){
-    return (((uint64_t)t) * (uint64_t)10000000) + ((uint64_t)116444736*1000000000);
-}
-
-uint64_t get_sys_time_interval(){
-    timespec  tim;
-    clock_gettime(CLOCK_MONOTONIC, &tim);
-    return (time_tToTimestamp( tim.tv_sec ) + tim.tv_nsec/100)/10000;
-}
-
-static void printRKNNTensor(rknn_tensor_attr *attr) {
-    printf("index=%d name=%s n_dims=%d dims=[%d %d %d %d] n_elems=%d size=%d fmt=%d type=%d qnt_type=%d fl=%d zp=%d scale=%f\n", 
-            attr->index, attr->name, attr->n_dims, attr->dims[3], attr->dims[2], attr->dims[1], attr->dims[0], 
-            attr->n_elems, attr->size, 0, attr->type, attr->qnt_type, attr->fl, attr->zp, attr->scale);
-}
-
-static unsigned char *load_model(const char *filename, int *model_size)
-{
-    FILE *fp = fopen(filename, "rb");
-    if(fp == nullptr) {
-        printf("fopen %s fail!\n", filename);
-        return NULL;
-    }
-    fseek(fp, 0, SEEK_END);
-    int model_len = ftell(fp);
-    unsigned char *model = (unsigned char*)malloc(model_len);
-    fseek(fp, 0, SEEK_SET);
-    if(model_len != fread(model, 1, model_len, fp)) {
-        printf("fread %s fail!\n", filename);
-        free(model);
-        return NULL;
-    }
-    *model_size = model_len;
-    if(fp) {
-        fclose(fp);
-    }
-    return model;
-}
-/*
-Mat get_net_work_img(Mat orig_img)
-{
-    cv::Mat img = orig_img.clone();
-    if(!orig_img.data) {
-        // printf("cv::imread %s fail!\n", img_path);
-        // return -1;
-    }
-    if(orig_img.cols != img_width || orig_img.rows != img_height) {
-        printf("resize %d %d to %d %d\n", orig_img.cols, orig_img.rows, img_width, img_height);
-        cv::resize(orig_img, img, cv::Size(img_width, img_height), (0, 0), (0, 0), cv::INTER_LINEAR);
-    }
-
-    return img;
-}*/
-
-/*-------------------------------------------
-                  Main Function
--------------------------------------------*/
 int main(int argc, char** argv)
 {
     // const int img_width = 224;
@@ -107,7 +49,7 @@ int main(int argc, char** argv)
     int model_len = 0;
     unsigned char *model;
 
-    const char *model_path = "test_model.rknn";
+    const char *model_path = argv[1];
     const char *img_path = argv[2];
     // const char *img_path2 = argv[3];
      // unsigned long start_time,end_load_model_time, stop_time;
@@ -116,15 +58,17 @@ int main(int argc, char** argv)
     // start_time = GetTickCount();
     long startt = get_sys_time_interval();
 
+    // Load image
+    cv::Mat img = cv::imread(img_path);
+    img = get_net_work_img(img);
+
 
     // Load RKNN Model
     model = load_model(model_path, &model_len);
     gettimeofday(&end_load_model_time, nullptr);
-
     // end_load_model_time = GetTickCount();
     long end_load_model = get_sys_time_interval();
     printf("end load model time:%ldms\n",end_load_model);
-
     ret = rknn_init(&ctx, model, model_len, 0);
     gettimeofday(&end_init_time, nullptr);
     // end_load_model_time = GetTickCount();
@@ -134,7 +78,7 @@ int main(int argc, char** argv)
         printf("rknn_init fail! ret=%d\n", ret);
         return -1;
     }
-/*
+
     // Get Model Input Output Info
     rknn_input_output_num io_num;
     ret = rknn_query(ctx, RKNN_QUERY_IN_OUT_NUM, &io_num, sizeof(io_num));
@@ -246,7 +190,6 @@ int main(int argc, char** argv)
     if(model) {
         free(model);
     }
-*/
     return 0;
 }
 
