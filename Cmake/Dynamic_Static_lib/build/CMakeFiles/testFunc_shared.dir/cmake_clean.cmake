9c5ce013815ba4cbf97274352e67d875ea20bbf0
LiuXiang199x <lx19961026@gmail.com>
2021-12-09 20:47:40 +0800

    2021/12/9

---
+++ Cmake/Dynamic_Static_lib/build/CMakeFiles/testFunc_shared.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testFunc_shared.dir/testFunc/testFunc.cc.o"
+  "../lib/libtestFunc.pdb"
+  "../lib/libtestFunc.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testFunc_shared.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ Cmake/Dynamic_Static_lib/build/CMakeFiles/testFunc_static.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testFunc_static.dir/testFunc/testFunc.cc.o"
+  "../lib/libtestFunc.pdb"
+  "../lib/libtestFunc.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testFunc_static.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
