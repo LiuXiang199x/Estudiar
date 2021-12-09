df0f4a28caea38d62d0a7d7f77f8b9dae69772ff
LiuXiang199x <lx19961026@gmail.com>
2021-12-09 20:39:53 +0800

    2021/12/9

---
+++ Cmake/Dynamic_Static_lib/include/testFunc.h
@@ -0,0 +1,6 @@
+#ifndef _TEST_FUNC_H_
+#define _TEST_FUNC_H_
+
+void func(int data);
+
+#endif
---
+++ Cmake/Dynamic_Static_lib/src/testFunc.cc
@@ -0,0 +1,10 @@
+#include<iostream>
+#include"testFunc.h"
+
+using namespace std;
+
+void func(int data){
+
+	cout << "I am in testFuncc" << endl;
+	cout << "data is = " << data << endl;
+}
