abf9e967bf4d2ab405493ba084abc56cb5a79140
LiuXiang199x <lx19961026@gmail.com>
2021-12-10 09:25:59 +0800

    2021/12/10

---
+++ Cmake/build_control_option1/build/src/CMakeFiles/main2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/main2.dir/main2.cc.o"
+  "../../bin/main2.pdb"
+  "../../bin/main2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/main2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
