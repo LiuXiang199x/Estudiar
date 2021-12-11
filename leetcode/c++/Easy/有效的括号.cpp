c1aa8801903725599555241c2c9ddf8bf558b003
LiuXiang199x <lx19961026@gmail.com>
2021-12-11 23:56:56 +0800

    2021/12/11

--- leetcode/c++/Easy/有效的括号.cpp
+++ leetcode/c++/Easy/有效的括号.cpp
@@ -26,3 +26,27 @@ int main(){
 
 	return 0;
 }
+
+// 推荐做法：
+class Solution{
+public:
+	bool isValid(string s){
+		int top = -1;
+		for(int i=0; i<s.length(); ++i){
+			if(top<0 || !isMatch(s[top], s[i])){
+				++top;
+				s[top] = s[i];		
+			}else{
+				--top;		
+			}
+		}
+		return top == -1;
+		
+	}
+	bool isMatch(char c1, char c2){
+		if(c1 == '(' && c2 == ")") return true;
+		if(c1 == '{' && c2 == "}") return true;
+		if(c1 == '[' && c2 == "]") return true;
+		return false;	
+	}
+};
