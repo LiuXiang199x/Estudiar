ffab282cec23aa1ff5aa53f485c28899205c8224
LiuXiang199x <lx19961026@gmail.com>
2022-01-06 21:05:57 +0800

    Lenet-5

---
+++ test/class/class1.py
@@ -0,0 +1,10 @@
+
+class test1():
+	
+	def printf():
+		print("22222")
+
+"""
+if __name__ == '__main__':
+	print(test1.printf())
+"""
---
+++ test/class/class2.py
@@ -0,0 +1,5 @@
+from class1 import test1
+
+if __name__ == "__main__":
+    a = test1
+    a.printf()
