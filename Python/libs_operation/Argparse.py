7f6dc267741c8a2ca2fd29d4a9fa246842ffff59
Marco <lx19961026@gmail.com>
2022-01-18 20:30:08 +0800

    Argparse

--- Python/libs_operation/Argparse.py
+++ Python/libs_operation/Argparse.py
@@ -13,10 +13,14 @@ def tutorial():
     parser = argparse.ArgumentParser()
     
     # 3增加属性：给xx实例增加一个aa属性 # xx.add_argument("aa")
+    # parser.add_argument("echo",help=" echo the string you use here!")
+    #向对象中添加相关命令行参数或选项,每一个add_argument方法对应一个参数或选项
     parser.add_argument("echo")         
     
     # 4属性给与args实例： 把parser中设置的所有"add_argument"给返回到args子类实例当中
     # 那么parser中增加的属性内容都会在args实例中，使用即可。
+    
+    #调用parse_args()方法进行解析
     args = parser.parse_args()
     parser.parse_args()
     
