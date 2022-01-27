b836fc013bd2a25152e119e79b09da71c55e1694
Marco <lx19961026@gmail.com>
2022-01-27 10:37:42 +0800

    generate_files

--- Python/test/test.py
+++ Python/test/test.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-names = ["frontier", "frontier+freemask"]
+names = ["test_data_frontiermask", "test_data_frontiermask_freemask"]
 
 for item in names:
-    os.mkdir("/home/agent/test_dataset/input_4/"+item)
+    os.mkdir("/home/agent/test_dataset/input_8_44pth/"+item)
     for i in range(131, 141):
-       os.mkdir("/home/agent/test_dataset/input_4/"+item+"/"+str(i))
-       open("/home/agent/test_dataset/input_4/"+item+"/"+str(i)+"/map.txt", "w")
-       open("/home/agent/test_dataset/input_4/"+item+"/"+str(i)+"/path.txt", "w")
+       os.mkdir("/home/agent/test_dataset/input_8_44pth/"+item+"/"+str(i))
+       open("/home/agent/test_dataset/input_8_44pth/"+item+"/"+str(i)+"/map.txt", "w")
+       open("/home/agent/test_dataset/input_8_44pth/"+item+"/"+str(i)+"/path.txt", "w")
--- Python/test/test_txt.py
+++ Python/test/test_txt.py
@@ -1,11 +1,28 @@
 import os
 
-f = open('/home/agent/Estudiar/Python/test/test.txt','w')
-lisss = ["aaa", "bbb", "ccc"]
+from importlib_metadata import re
+import torch
 
-for a in lisss:
-    filename = a+"/"+"image"
-    for i in range(9):
-        input_txt = filename + " " + str(i) + "\n"
-        f.write(input_txt)
-f.close()
+
+def save2txt():
+    f = open('/home/agent/Estudiar/Python/test/test.txt','w')
+    lisss = ["aaa", "bbb", "ccc"]
+
+    for a in lisss:
+        filename = a+"/"+"image"
+        for i in range(9):
+            input_txt = filename + " " + str(i) + "\n"
+            f.write(input_txt)
+    f.close()
+    
+def read2txt():
+    f = open('/home/agent/Estudiar/Python/test/test.txt','r')
+    for i in f:
+        print(i)
+        print(len(i))
+    f.close()
+    
+a = torch.tensor(2)
+print(a)
+print(int(a))
+print(a.int())
