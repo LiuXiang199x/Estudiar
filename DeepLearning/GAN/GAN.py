caab4ebbde496bfe64de2ff697b0bfb78fa5aa49
marco <lx19961026@gmail.com>
2022-02-05 23:04:34 +0800

    2/5

---
+++ DeepLearning/GAN/GAN.py
@@ -0,0 +1,4 @@
+import numpy as np
+a = np.random.uniform(1, 2, size=batch_size)[:, np.newaxis]
+base = np.linspace(-1, 1, data_dim)[np.newaxis, :].repeat(batch_size, axis=0)
+y = a * np.power(base, 2) + (a-1)
