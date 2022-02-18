89010af16cf134eedc893b50b9de5a6e59cba4d3
Marco <lx19961026@gmail.com>
2022-02-18 18:49:36 +0800

    .

--- DeepLearning/ImgAugmenters/examples_Basics/AddBlurs.py
+++ DeepLearning/ImgAugmenters/examples_Basics/AddBlurs.py
@@ -7,24 +7,32 @@ import cv2 as cv
 image = cv.imread("/home/agent/Estudiar/DeepLearning/ImgAugmenters/bedroom.jpg")
 
 
-def add_blur(img_path):
+def add_blur(img_path, save_path):
+    
     img2 = [img_path for _ in range(20)]
+    
+    # input img 2 seq with list
     seq = iaa.Sequential([
         iaa.OneOf([
-            # iaa.MotionBlur(k=20, angle=0),
-            # iaa.MotionBlur(k=20, angle=45),
-            iaa.MotionBlur(k=120, angle=90),
+            iaa.MotionBlur(k=20, angle=0),
+            iaa.MotionBlur(k=20, angle=45),
+            iaa.MotionBlur(k=20, angle=90),
             
-            #iaa.LinearContrast(0.5),
-            #iaa.Multiply(0.5,per_channel=0.5),
-            #iaa.Multiply(2,per_channel=0.5),
+            iaa.LinearContrast(0.5),
+            iaa.Multiply(0.5,per_channel=0.5),
+            iaa.Multiply(2,per_channel=0.5),
         ]),
     ])
     img_aug = seq(images=img2)
+    
+    # visulization
     ia.imshow(np.hstack(img_aug))
     for i in range(np.array(img_aug).shape[0]):
         img = img_aug[i]
         cv.imshow("qweqwe", img)
         cv.waitKey(0)
+        
+        # save img
+        cv.imwrite(img, save_path)
         
 add_blur(img_path=image)
