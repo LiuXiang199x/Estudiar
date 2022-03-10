954e2a9739ca9ac0b0e6d00620ef59f6daf8c5c9
Marco <lx19961026@gmail.com>
2022-03-10 14:58:09 +0800

    opencv

--- DeepLearning/Project/detect/src/as.py
+++ DeepLearning/Project/detect/src/as.py
@@ -1,1 +1,6 @@
 import torch
+import cv2 as cv 
+
+img = cv.imread("1")
+cv.imshow("as", img)
+cv.waitKey(0)
--- OpenCV/Tutorial/Load_save.py
+++ OpenCV/Tutorial/Load_save.py
@@ -1,7 +1,9 @@
 import cv2 as cv
 
-img = cv.imread("Image/opencv.jpeg")
-
+img = cv.imread("/home/agent/1")
+cv.imshow("src", img)
+out = cv.waitKey(0)
+cv.imwrite("/home/agent/11.jpg", img)
 b, g, r = cv.split(img)
 img[:, :, 0] = 0
 img[:, :, 1] = 0
