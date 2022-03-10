2b0216a15dc7cd25a70d3a721431cfd15c4bf5d9
Marco <lx19961026@gmail.com>
2022-03-10 16:09:46 +0800

    cmake_c=+

---
+++ C++/Cmake_C++/build/CMakeFiles/test_eigen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "../bin/test_eigen"
+  "../bin/test_eigen.pdb"
+  "CMakeFiles/test_eigen.dir/src/main.cc.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/test_eigen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
