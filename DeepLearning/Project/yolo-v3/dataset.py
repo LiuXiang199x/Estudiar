33dd5c04667ac830f72125c852ce822fcfcebfdd
marco <lx19961026@gmail.com>
2022-03-20 23:11:24 +0800

    proj_yolo

--- DeepLearning/Project/yolo-v3/dataset.py
+++ DeepLearning/Project/yolo-v3/dataset.py
@@ -37,6 +37,8 @@ class YoloDataSet(Dataset):
         # 比如：640*416，我们会选max生成640*640，然后多余的区域用黑色去填充
         # 虽然改了图像大小，但是坐标其实不用变，因为原点是左上角，而且resize图像也是从左上角开始粘贴的。
         img=make_416_image(os.path.join('/home/marco/Estudiar/DeepLearning/Project/yolo-v3/dataset/img',temp_data[0]))
+        
+        # 计算缩放比
         w,h=img.size
         case=416/w
         img=img.resize((DATA_WIDTH,DATA_HEIGHT))
@@ -46,13 +48,19 @@ class YoloDataSet(Dataset):
             labels[feature_size]=np.zeros(shape=(feature_size,feature_size,3,5+CLASS_NUM))
 
             for box in boxes:
+                # 得到原图上的坐标
                 cls,cx,cy,w,h=box
+                # 进行缩放
                 cx, cy,w,h=cx*case,cy*case,w*case,h*case
+
+                # 计算偏移量，中心点的偏移量
                 _x,x_index=math.modf(cx*feature_size/DATA_WIDTH)
                 _y,y_index=math.modf(cx*feature_size/DATA_HEIGHT)
                 for i,antor in enumerate(_antors):
                     area=w*h
                     iou=min(area,ANTORS_AREA[feature_size][i])/max(area,ANTORS_AREA[feature_size][i])
+                    
+                    # 计算 w和h 的偏移量，就是用真实的w和h去除以建议的w和h
                     p_w, p_h = w / antor[0], h / antor[1]
                     labels[feature_size][int(y_index),int(x_index),i]=np.array([iou,_x,_y,np.log(p_w),np.log(p_h),*one_hot(CLASS_NUM,int(cls))])
 
@@ -60,7 +68,7 @@ class YoloDataSet(Dataset):
 
 if __name__ == '__main__':
     dataset=YoloDataSet()
-    print(dataset[0][3].shape)
-    print(dataset[0][0].shape)
-    print(dataset[0][1].shape)
-    print(dataset[0][2].shape)
+    print(dataset[0][3].shape)  # torch.Size([3, 416, 416])
+    print(dataset[0][0].shape)  # (13, 13, 3, 8)
+    print(dataset[0][1].shape)  # (26, 26, 3, 8)
+    print(dataset[0][2].shape)  # (52, 52, 3, 8)
