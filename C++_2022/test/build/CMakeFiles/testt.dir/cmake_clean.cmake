a163ce92ade20c88e2a7fde543e6209ffb32ad0b
Xiang Liu <lx19961026@gmail.com>
2022-04-13 15:32:43 +0800

    .

---
+++ C++_2022/test/build/CMakeFiles/testt.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/testt.dir/src/main.cc.o"
+  "testt.pdb"
+  "testt"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/testt.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
