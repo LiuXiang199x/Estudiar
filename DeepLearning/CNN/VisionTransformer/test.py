7d073fa2667dd9f9f78323ffe1adf45cf73909ef
marco <lx19961026@gmail.com>
2022-06-05 14:41:21 +0800

    .

--- DeepLearning/CNN/VisionTransformer/test.py
+++ DeepLearning/CNN/VisionTransformer/test.py
@@ -1,3 +1,4 @@
+from socket import ALG_SET_AEAD_ASSOCLEN
 import numpy as np
 import cv2 as cv
 import torch
@@ -6,3 +7,5 @@ import torch
 #### transformer 论文阅读 ####
 ## 95后应届生，机器学习算法工程师 ###
 ###### 先学一会儿，晚点聊天 ########
+
+ALG_SET_AEAD_ASSOCLEN
