23f8e3d71712369b178db9a40c7e06b286884587
marco <lx19961026@gmail.com>
2022-06-19 14:29:51 +0800

    1

--- DeepLearning/CNN/VisionTransformer/tett/a.py
+++ DeepLearning/CNN/VisionTransformer/tett/a.py
@@ -1,8 +1,31 @@
 import numpy as np
 import csv
 
-file_pth = "/home/marco/下载/facebook-contest_export.csv"
-f = open(file_pth)
-f = csv.DictReader(f)
-for row in f:
-    print(row)
+def run_(file_pth):
+    f = open(file_pth)
+    f = csv.DictReader(f)
+    wordsCounterList = {}
+
+    for row in f:
+        phrase_ = row["Details"].lower()
+        words_lst = phrase_.replace(",","").split(" ")
+        for word in words_lst:
+            if word not in wordsCounterList.keys():
+                wordsCounterList[word] = 1
+            else:
+                wordsCounterList[word] += 1
+    print("===============> done")
+    print(wordsCounterList.keys())
+    result_ = {}
+    for key_name in wordsCounterList.keys():
+        if wordsCounterList[key_name] > 1:
+            result_[key_name] = wordsCounterList[key_name]
+    # print(wordsCounterList)
+    result_ = sorted(result_.items(),key = lambda result_:result_[1],reverse= True)
+    return result_
+
+if __name__ == "__main__":
+    csv_path = "/home/marco/下载/facebook-contest_export.csv"
+    
+    final_output = run_(csv_path)
+    print(final_output)
