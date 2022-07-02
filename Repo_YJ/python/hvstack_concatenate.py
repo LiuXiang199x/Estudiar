bc03b3fb178903cf4509a8a0c230bbb595561866
marco <lx19961026@gmail.com>
2022-07-02 14:47:31 +0800

    stack

--- Repo_YJ/python/hvstack_concatenate.py
+++ Repo_YJ/python/hvstack_concatenate.py
@@ -3,6 +3,7 @@ import torch
 import torch.nn as nn
 
 
+# 最全能，根据指定的维度，对一个元组、列表中的list或者ndarray进行连接
 def concatenate():
     a1 = np.array([[1,2,3]])
     a2 = np.array([[3,3,3]])
@@ -12,5 +13,22 @@ def concatenate():
     a3 = np.array([[2,3,4],[1,2,3]])
     print(a3)
     print(a3.T)
+
+# 将一堆数组的数据按照指定的维度进行堆叠。
+def stack():
+    a1 = np.array([[1,2,3]])
+    a2 = np.array([[3,3,3]])
+
+    print(np.stack((a1,a2), axis=1))
     
+    print("\n======== vstack ========")
+    # 它是垂直（按照行顺序）的把数组给堆叠起来。
+    print(np.vstack((a1, a2)))
+
+    print("\n======== hstack ========")
+    # 它其实就是水平(按列顺序)把数组给堆叠起来
+    print(np.hstack((a1, a2)))
+
+
 concatenate()
+stack()
