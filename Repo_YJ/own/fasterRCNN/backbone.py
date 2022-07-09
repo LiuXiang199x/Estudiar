432eab1e11e52b1bf362326bf3242b80b0295179
marco <lx19961026@gmail.com>
2022-07-09 18:21:48 +0800

    resnet

--- Repo_YJ/own/fasterRCNN/backbone.py
+++ Repo_YJ/own/fasterRCNN/backbone.py
@@ -3,4 +3,9 @@ import torch.nn as nn
 import torchvision
 from torchvision.models.detection import faster_rcnn
 
-
+b = torchvision.models.resnet18()
+c = torchvision.models.resnet50()
+d = torchvision.models.mobilenet_v2()
+print(b)
+model = faster_rcnn.FasterRCNN(backbone=b)
+print(model)
--- Repo_YJ/own/maskRCNN/backbone.py
+++ Repo_YJ/own/maskRCNN/backbone.py
@@ -2,3 +2,7 @@ import torch
 import torchvision
 import torch.nn as nn
 from torchvision.models.detection import mask_rcnn
+
+
+model = mask_rcnn.MaskRCNN()
+print(model)
