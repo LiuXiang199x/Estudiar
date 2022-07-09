8376cce819215b6dd441ca267a1a336374162d92
marco <lx19961026@gmail.com>
2022-07-09 17:43:23 +0800

    mask rcnn

--- Repo_YJ/own/fasterRCNN/backbone.py
+++ Repo_YJ/own/fasterRCNN/backbone.py
@@ -2,3 +2,5 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchvision.models.detection import faster_rcnn
+
+
---
+++ Repo_YJ/own/maskRCNN/backbone.py
@@ -0,0 +1,4 @@
+import torch
+import torchvision
+import torch.nn as nn
+from torchvision.models.detection import mask_rcnn
