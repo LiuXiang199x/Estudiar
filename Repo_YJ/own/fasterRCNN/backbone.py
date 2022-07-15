4ff2d95eade6fe5a3d090ba0ea9a9fe0b45ec767
marco <lx19961026@gmail.com>
2022-07-16 02:10:34 +0800

    1

--- Repo_YJ/own/fasterRCNN/backbone.py
+++ Repo_YJ/own/fasterRCNN/backbone.py
@@ -2,7 +2,7 @@ import torch
 import torch.nn as nn
 import torchvision
 from torchvision.models.detection import faster_rcnn
-
+from torch.utils.data import DataLoader
 
 model = faster_rcnn.FasterRCNN()  # 必须要指定一个backbone
 print(model)
