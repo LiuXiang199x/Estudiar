9999f28801fdd9580b1095e0532e5a5be4d15136
marco <lx19961026@gmail.com>
2022-07-17 01:40:33 +0800

    CE

--- Repo_YJ/own/CrossEntropy/CE.py
+++ Repo_YJ/own/CrossEntropy/CE.py
@@ -1,2 +1,18 @@
 import torchvision
 import torch
+import torch.nn as nn
+
+def testSoftmaxSigmoid():
+    activation = nn.Softmax()
+    activation2 = nn.Sigmoid()
+
+    inputs = torch.tensor([1., 2.])
+    print("===========> test for sigmoid")
+    print(activation2(inputs))
+    print(1/(1+torch.exp(torch.tensor(-1.))))
+    print(1/(1+torch.exp(torch.tensor(-2.))))
+
+    print("===========> test for softmax")
+    print(activation(inputs))
+    print(torch.exp(torch.tensor(1.))/(torch.exp(torch.tensor(1.))+torch.exp(torch.tensor(2.))))
+    print(torch.exp(torch.tensor(2.))/(torch.exp(torch.tensor(1.))+torch.exp(torch.tensor(2.))))
