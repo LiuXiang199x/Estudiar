8f1e4aa389129c11f95022b6b172548e65209e5f
marco <lx19961026@gmail.com>
2022-07-24 20:54:54 +0800

    !

---
+++ Repo_YJ/own/AttentionMechanism/SE.py
@@ -0,0 +1,19 @@
+import torch
+import torch.nn as nn
+
+class SeModule(nn.Module):
+    def __init__(self, in_size, reduction=4):
+        super(SeModule, self).__init__()
+        self.se = nn.Sequential(
+            nn.AdaptiveAvgPool2d(1),
+            nn.Conv2d(in_size, in_size // reduction, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size // reduction),
+ 
+            nn.PReLU(in_size//reduction),
+            nn.Conv2d(in_size // reduction, in_size, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.BatchNorm2d(in_size),
+            nn.PReLU(in_size)
+        )
+ 
+    def forward(self, x):
+        return x * self.se(x)
