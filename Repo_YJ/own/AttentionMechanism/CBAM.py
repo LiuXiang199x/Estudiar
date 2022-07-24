e09035270e1de0d86268ddabb7fd441685781843
marco <lx19961026@gmail.com>
2022-07-24 20:54:17 +0800

    !

---
+++ Repo_YJ/own/AttentionMechanism/CBAM.py
@@ -0,0 +1,13 @@
+import torch
+import torch.nn as nn
+
+
+class CBAM(nn.Module):
+    def __init__(self, inplanes, ratio=16, kernel_size=7):
+        super(CBAM, self).__init__()
+        self.ca = ChannelAttention(inplanes, ratio)
+        self.sa = SpatialAttention(kernel_size)
+    def forward(self, x):
+        out = x*self.ca(x)
+        result = out*self.sa(out)
+        return result
